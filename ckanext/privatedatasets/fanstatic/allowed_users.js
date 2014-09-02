2b6c46805130399be6cc9858210f24fa7d096e2c
Aitor Magán <amagan@conwet.com>
2014-09-02 17:52:55 +0200

    Add GNU Affero License

--- ckanext/privatedatasets/fanstatic/allowed_users.js
+++ ckanext/privatedatasets/fanstatic/allowed_users.js
@@ -1,3 +1,24 @@
+/*
+ * (C) Copyright 2014 CoNWeT Lab., Universidad Politécnica de Madrid
+ *
+ * This file is part of CKAN Private Dataset Extension.
+ *
+ * CKAN Private Dataset Extension is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * CKAN Private Dataset Extension is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CKAN Private Dataset Extension Platform. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
+
 /* Dataset allowed_users, searchable and acquire_url toggler
  * allowed_users, acquire_url and searchable can only be active when a 
  * user attempts to create a private dataset
